1132555e5e5a036ad47a6159639b7549c5035db4
Marcos Gabarda <marcosgabarda@gmail.com>
2017-06-22 23:44:28 +0200

    signup success

--- pycones/reviewers/urls.py
+++ pycones/reviewers/urls.py
@@ -2,10 +2,13 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from pycones.reviewers.views import ReviewerSignUpView, ReviewView, ReviewListView
 
 urlpatterns = [
+    url(r'^sign-up/success/$',
+        TemplateView.as_view(template_name="reviewers/sign_up_success.html"), name="sign-up-success"),
     url(r'^sign-up/$', ReviewerSignUpView.as_view(), name="sign-up"),
     url(r'^(?P<pk>\d+)/$', ReviewView.as_view(), name="details"),
     url(r'^$', ReviewListView.as_view(), name="list"),
