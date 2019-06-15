7a7cf4bd7c3e89eb0869328b18942f3518ba4aec
Edu Herraiz <eherraiz@apsl.net>
2019-06-15 19:27:37 +0200

    TRavis4

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ echo "Pull new code from repository"
 git pull origin master
 echo "Building new docker image"
 docker-compose build
-echo "Install npm dependencies on new image"
-docker-compose run webapp npm install
-echo "Run npm build on new image"
-docker-compose run webapp npm run build
+# Not necessary by the moment:
+# echo "Install npm dependencies on new image"
+# docker-compose run webapp npm install
+# echo "Run npm build on new image"
+# docker-compose run webapp npm run build
 echo "Collectstatic for new image"
 docker-compose run webapp python3 /app/manage.py collectstatic --noinput
 echo "Compilemessages"
