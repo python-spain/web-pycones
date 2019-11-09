984f6fd64768d86a7fd4ca87895e2062b373976f
Edu Herraiz <eherraiz@apsl.net>
2019-11-09 17:14:23 +0100

    Remove old docker and env files

--- compose/webapp/gunicorn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Not necessary 
-#npm install
-#npm run build
-#python3 /app/manage.py collectstatic --noinput
-#python3 /app/manage.py compilemessages
-#python3 /app/manage.py migrate
-/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
